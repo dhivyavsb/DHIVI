1514bdf402f07f9dcf41321164604ad087dce21d
dhivyavsb <31505815+dhivyavsb@users.noreply.github.com>
2018-03-15 16:47:57 +0530

    Create player 39.c

---
+++ player 39.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+#include<math.h>
+int main()
+{
+	int d,i;
+	scanf("%d",&d);
+            for(i=1;i<=d;i++)	 
+            if(pow(2,i)==d) 
+            printf("yes");
+	return 0;
+}
